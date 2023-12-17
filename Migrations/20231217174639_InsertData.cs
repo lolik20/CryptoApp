f401d12971dd3e19c3a864aed3a3a23a90431aa6
lolik20 <mailsteama230@gmail.com>
2023-12-18 00:51:07 +0700

    Insert data script

---
+++ Migrations/20231217174639_InsertData.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CryptoExchange.Migrations
+{
+    /// <inheritdoc />
+    public partial class InsertData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"INSERT INTO public.""Currencies""(
+	""Id"", ""Type"", ""Code"", ""Name"")
+	VALUES (1, 0, 'BTC', 'Bitcoin'),(2,2,'USD','US Dollar')");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
