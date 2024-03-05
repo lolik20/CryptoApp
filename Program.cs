9c818c66b2092bf14ae772fb86b3508da4a75d72
lolik20 <mailsteama230@gmail.com>
2024-03-05 20:15:59 +0700

    Production swagger

--- Program.cs
+++ Program.cs
@@ -44,11 +44,11 @@ builder.Services.AddMediatR(cfg =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+//if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+//}
 app.UseCors(x => x.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
 app.UseMiddleware<ExceptionHandlingMiddleware>();
 
